e7152f4878636a95a1f0f6cb220efbe9d75c543f
HI$AM <138756079+HishamEltayb@users.noreply.github.com>
2024-10-06 16:01:49 +0000

    from the linux

--- test.cpp
+++ test.cpp
@@ -38,3 +38,11 @@ VBoxManage setextradata "macOS Catalina" "VBoxInternal/Devices/efi/0/Config/DmiS
 VBoxManage setextradata "macOS Catalina" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Iloveapple"
 VBoxManage setextradata "macOS Catalina" "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
 VBoxManage setextradata "macOS Catalina" "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
+
+
+
+sudo dscl . -create /Users/aliali;sudo dscl . -create /Users/aliali UserShell /bin/bash;sudo dscl . -create /Users/aliali RealName "Aliali User";sudo dscl . -create /Users/aliali UniqueID "512"  # Ensure this is unique;sudo dscl . -create /Users/aliali PrimaryGroupID 20;sudo dscl . -create /Users/aliali NFSHomeDirectory /Users/aliali;sudo dscl . -passwd /Users/aliali aliali;sudo createhomedir -c -u aliali
+
+
+
+https://chatgpt.com/share/6702b433-caf4-8006-a208-4d28f4edd81b
